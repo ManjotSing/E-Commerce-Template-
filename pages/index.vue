33ccb4981bbfc3db6e44a40de59dd1e8246c1346
manjotsingh <manjot.singh@bizmerlin.com>
2020-06-11 12:34:18 +0530

    Front End Template

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+      <NavBar />
+      <Carousel />
+      <Lotteries />
+  </div>
+</template>
+
+<script>
+import NavBar from '~/components/NavBar.vue'
+import Carousel from '~/components/Carousel.vue'
+import Lotteries from '~/components/Lotteries.vue'
+export default {
+  components: {
+    NavBar,
+    Carousel,
+    Lotteries
+  }
+}
+</script>
+
+<style>
+</style>
